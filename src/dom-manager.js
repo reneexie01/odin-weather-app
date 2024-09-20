57c057b25a66d0930ed1214577e9748ca1edf2a4
Renee <156127959+reneexie01@users.noreply.github.com>
2024-09-21 06:59:01 +1000

    Added the weatherOutput onto the DOM

--- src/dom-manager.js
+++ src/dom-manager.js
@@ -15,7 +15,19 @@ const domManager = (function DomManager() {
             try {
                 let weatherOutput = await weatherAPI.getWeather(inputFieldValue);
                 console.log(weatherOutput);
-
+                weatherContainer.innerHTML = `
+                <ul>
+                    <li class="current-output" id="current-date">Date: ${weatherOutput.date}</li>
+                    <li class="current-output" id="current-description">Description: ${weatherOutput.description}</li>
+                    <li class="current-output" id="current-location">Location: ${weatherOutput.resolvedAddress}</li>
+                    <li class="current-output" id="current-timezone">Timezone: ${weatherOutput.timezone}</li>
+                    <li class="current-output" id="current-temperature">Current Temperature: ${weatherOutput.temp}</li>
+                    <li class="current-output" id="current-feelslike">Feels Like: ${weatherOutput.feelsLike}</li>
+                    <li class="current-output" id="todays-max">Temperature (Max): ${weatherOutput.tempMax}</li>
+                    <li class="current-output" id="todays-min">Temperature (Min): ${weatherOutput.tempMin}</li>
+                    <li class="current-output" id="current-precipitation">Precipitation:${weatherOutput.precip}</li>
+                </ul>
+                `
                 //TODO: Add the weatherOutput to the weatherContainer
             } catch (err) {
                 console.log(err);
