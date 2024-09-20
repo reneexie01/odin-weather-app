b9d755476a908b9ac04ead6d940f45a28926227d
Renee <156127959+reneexie01@users.noreply.github.com>
2024-09-21 07:43:30 +1000

    Added next three day's weather and printed on DOM

--- src/dom-manager.js
+++ src/dom-manager.js
@@ -3,19 +3,19 @@ import { weatherAPI } from "./weather-api";
 export { domManager };
 
 const domManager = (function DomManager() {
+  const inputField = document.querySelector(".location");
+  const submitButton = document.querySelector(".submit");
+  const todayWeatherContainer = document.querySelector(".weather-today");
+  const futureWeatherContainer = document.querySelector(".weather-future");
 
-    const inputField = document.querySelector(".location");
-    const submitButton = document.querySelector(".submit");
-    const weatherContainer = document.querySelector(".weather-today");
-    
-    const locationButton = () => {
-        submitButton.addEventListener("click", async () => {
-            const inputFieldValue = inputField.value;
+  const locationButton = () => {
+    submitButton.addEventListener("click", async () => {
+      const inputFieldValue = inputField.value;
 
-            try {
-                let weatherOutput = await weatherAPI.getWeather(inputFieldValue);
-                console.log(weatherOutput);
-                weatherContainer.innerHTML = `
+      try {
+        let weatherOutput = await weatherAPI.getWeather(inputFieldValue);
+        console.log(weatherOutput);
+        todayWeatherContainer.innerHTML = `
                 <ul>
                     <li class="current-output" id="current-date">Date: ${weatherOutput.date}</li>
                     <li class="current-output" id="current-description">Description: ${weatherOutput.description}</li>
@@ -25,16 +25,34 @@ const domManager = (function DomManager() {
                     <li class="current-output" id="current-feelslike">Feels Like: ${weatherOutput.feelsLike}</li>
                     <li class="current-output" id="todays-max">Temperature (Max): ${weatherOutput.tempMax}</li>
                     <li class="current-output" id="todays-min">Temperature (Min): ${weatherOutput.tempMin}</li>
-                    <li class="current-output" id="current-precipitation">Precipitation:${weatherOutput.precip}</li>
+                    <li class="current-output" id="current-precipitation">Precipitation: ${weatherOutput.precip}</li>
                 </ul>
-                `
-                //TODO: Add the weatherOutput to the weatherContainer
-            } catch (err) {
-                console.log(err);
-            }
-          })
-    }
+                `;
+      } catch (err) {
+        console.log(err);
+      }
 
+      try {
+        let futureWeatherOutput =
+          await weatherAPI.getWeeklyWeather(inputFieldValue);
+        console.log(futureWeatherOutput);
+        futureWeatherOutput.forEach((item, index) => {
+          const div = document.createElement("div");
+          div.innerHTML = `
+                    <ul>
+                        <li class="day${index + 1}-output" id="day${index + 1}-date">Date: ${item.date}</li>
+                        <li class="day${index + 1}-output" id="day${index + 1}-max">Temperature (Max): ${item.tempMax}</li>
+                        <li class="day${index + 1}-output" id="day${index + 1}-min">Temperature (Min): ${item.tempMin}</li>
+                        <li class="day${index + 1}-output" id="day${index + 1}-description">Description: ${item.description}</li>
+                    </ul>
+                    `;
+          futureWeatherContainer.appendChild(div);
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    });
+  };
 
-    return { locationButton };
+  return { locationButton };
 })();
--- src/weather-api.js
+++ src/weather-api.js
@@ -27,26 +27,77 @@ const weatherAPI = (function WeatherAPI() {
       const currentConditionsDescription = currentConditionsOutput.conditions;
 
       let weatherOutput = {
-          resolvedAddress: resolvedAddress,
-          timezone: timeZone,
-          date: todaysDate,
-          temp: currentTemperature,
-          feelsLike: currentFeelsLikeTemperature,
-          tempMax: todaysTempMax,
-          tempMin: todaysTempMin,
-          precip: todaysPrecip,
-          description: currentConditionsDescription,
-        };
-  
-        //TODO: Try and get the week's data
-      
-        //TODO: Add the weather to the DOM using the div
-      
+        resolvedAddress: resolvedAddress,
+        timezone: timeZone,
+        date: todaysDate,
+        temp: currentTemperature,
+        feelsLike: currentFeelsLikeTemperature,
+        tempMax: todaysTempMax,
+        tempMin: todaysTempMin,
+        precip: todaysPrecip,
+        description: currentConditionsDescription,
+      };
       return weatherOutput;
     } catch (err) {
       console.log(err);
     }
   };
 
-  return { getWeather };
+  const getWeeklyWeather = async function getWeeklyWeather(location) {
+    try {
+      const response = await fetch(
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=NDNCH7XP9PE4PXWQB9T3R7ED5`,
+        { mode: "cors" }
+      );
+
+      const weatherData = await response.json();
+
+      const day1Output = weatherData.days[1];
+      const day1Date = day1Output.datetime;
+      const maxTemp1 = convertToCelcius(day1Output.tempmax);
+      const minTemp1 = convertToCelcius(day1Output.tempmin);
+      const description1 = day1Output.conditions;
+
+      let weatherOutput1 = {
+        date: day1Date,
+        tempMax: maxTemp1,
+        tempMin: minTemp1,
+        description: description1,
+      };
+
+      const day2Output = weatherData.days[2];
+      const day2Date = day2Output.datetime;
+      const maxTemp2 = convertToCelcius(day2Output.tempmax);
+      const minTemp2 = convertToCelcius(day2Output.tempmin);
+      const description2 = day2Output.conditions;
+
+      let weatherOutput2 = {
+        date: day2Date,
+        tempMax: maxTemp2,
+        tempMin: minTemp2,
+        description: description2,
+      };
+
+      const day3Output = weatherData.days[3];
+      const day3Date = day3Output.datetime;
+      const maxTemp3 = convertToCelcius(day3Output.tempmax);
+      const minTemp3 = convertToCelcius(day3Output.tempmin);
+      const description3 = day3Output.conditions;
+
+      let weatherOutput3 = {
+        date: day3Date,
+        tempMax: maxTemp3,
+        tempMin: minTemp3,
+        description: description3,
+      };
+
+      let futureWeather = [weatherOutput1, weatherOutput2, weatherOutput3];
+
+      return futureWeather;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return { getWeather, getWeeklyWeather };
 })();
