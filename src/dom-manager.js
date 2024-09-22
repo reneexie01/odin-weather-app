f7cad2c80bc081f46c0e31613f17d315107a2fdd
Renee <156127959+reneexie01@users.noreply.github.com>
2024-09-23 06:56:14 +1000

    Added basic loading notice when fetching API

--- src/dom-manager.js
+++ src/dom-manager.js
@@ -7,13 +7,15 @@ const domManager = (function DomManager() {
   const submitButton = document.querySelector(".submit");
   const todayWeatherContainer = document.querySelector(".weather-today");
   const futureWeatherContainer = document.querySelector(".weather-future");
-
-//TODO: Need to clear DOM every time the button is clicked (for future weather data)
+  const loadingContainer = document.querySelector(".loading");
 
   const locationButton = () => {
     submitButton.addEventListener("click", async () => {
-      const inputFieldValue = inputField.value;
+      loadingContainer.classList.remove("hidden");
 
+      const inputFieldValue = inputField.value;
+      todayWeatherContainer.innerHTML = "";
+      futureWeatherContainer.innerHTML = "";
       try {
         let weatherOutput = await weatherAPI.getWeather(inputFieldValue);
         console.log(weatherOutput);
@@ -21,14 +23,14 @@ const domManager = (function DomManager() {
                 <div class="current-output">
                     <div id="current-temperature">${weatherOutput.temp}</div>
                     <div id="current-description">${weatherOutput.description}</div>
-                    <div id="current-date">${weatherOutput.date}</div>
+                    <div id="current-date">Date: ${weatherOutput.date}</div>
                     <div id="current-location">${weatherOutput.resolvedAddress} (${weatherOutput.timezone})</div>
                     <div id="current-feelslike">Feels Like: ${weatherOutput.feelsLike}</div>
                     <div id="todays-minmax">
                         <div id="min"><span class="bold">Min:</span> ${weatherOutput.tempMin}</div> 
                         <div id="max"><span class="bold">Max:</span> ${weatherOutput.tempMax}</div>
                     </div>
-                    <div id="current-precipitation">Precipitation: ${weatherOutput.precip}</div>
+                    <div id="current-precipitation">Precipitation: ${weatherOutput.precip}%</div>
                 </div>
                 `;
       } catch (err) {
@@ -45,7 +47,7 @@ const domManager = (function DomManager() {
           div.innerHTML = `
                     <div class="day${index+1}-output">
                         <div id="day${index + 1}-date">Date: ${item.date}</div>
-                        <div id="day${index + 1}-minmax minmax">
+                        <div id="day${index + 1}-minmax" class="minmax">
                             <div id="min"><span class="bold">Min:</span> ${item.tempMin}</div> 
                             <div id="max"><span class="bold">Max:</span> ${item.tempMax}</div>
                         </div>
@@ -53,6 +55,7 @@ const domManager = (function DomManager() {
                     </div>
                     `;
           futureWeatherContainer.appendChild(div);
+          loadingContainer.className = "hidden";
         });
       } catch (err) {
         console.log(err);
